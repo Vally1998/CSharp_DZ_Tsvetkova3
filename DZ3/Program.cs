4f7b29e1de1d53e6e7f304e64fd7f8f21e014ac9
Vally1998 <164740279+Vally1998@users.noreply.github.com>
2024-05-01 17:17:10 +0300

    DZ3.3

---
+++ DZ3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3
+// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+// Решение
+void Main()
+{
+    int[] Array = { 4, 3, 0, 75, 16, 29, 34, 5, 7, 10 };
+    PrintArrayReverse(Array, Array.Length - 1);
+}
+
+void PrintArrayReverse(int[] newArray, int i)
+{
+    if (i >= 0) 
+    {
+        System.Console.Write(newArray[i] + " ");
+        PrintArrayReverse(newArray, i - 1);
+    }
+}
+
+Main();
