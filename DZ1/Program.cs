268dc78139fe23e481f98d5dec3e58bfd5bf43c0
Vally1998 <164740279+Vally1998@users.noreply.github.com>
2024-05-01 17:13:49 +0300

    DZ1.3

---
+++ DZ1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+// Решение
+
+void Main()
+{
+    int number1 = ReadInt("Введите число M: ");
+    int number2 = ReadInt("Введите число N: ");
+    PrintNumbers(number1, number2);
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int M, int N)
+{
+    if (M <= N)
+    {
+        System.Console.Write(M + " ");
+        PrintNumbers(M + 1, N);
+    } 
+}
+Main();
+
